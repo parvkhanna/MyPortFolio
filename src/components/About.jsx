bed61f19c33ca4fddae606b4535add53c4b3e3f2
Parv Khanna <mymac@Parvs-Laptop.local>
2024-09-21 13:08:57 +0530

    First commit

---
+++ src/components/About.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+import { styles } from '../styles';
+import { services } from '../constants';
+import { fadeIn, textVariant } from '../utils/motion';
+import { SectionWrapper } from '../hoc';
+
+const ServiceCard = ({ index, title, icon }) => {
+  return (
+    <motion.div
+      variants={fadeIn('right', 'spring', 0.5 * index, 0.75)}
+      className="xs:w-[250px] w-full card-gradient p-[1px] rounded-[20px] shadow-card">
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className="bg-jetLight rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col">
+        <img src={icon} alt={title} className="w-16 h-16 object-contain" />
+        <h3 className="text-taupe text-[18px] font-bold text-center">
+          {title}
+        </h3>
+      </div>
+    </motion.div>
+  );
+};
+
+const About = () => {
+  return (
+    <div className="-mt-[6rem]">
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn('', '', 0.1, 1)}
+        className="mt-4 text-taupe text-[18px] max-w-3xl leading-[30px]">
+        Hey there! I'm Parv Khanna, your go-to QA Automation Engineer with a knack for breaking things (in the best way possible)! With 4+ years of experience, I’ve been diving deep into the world of software testing, automating everything from web apps to APIs. Whether it's rolling out slick Selenium scripts or making sure CI/CD pipelines run like butter, I’ve got it covered.
+        From auction houses to finance apps, I’ve been the testing ninja behind the scenes—finding bugs, squashing ‘em, and making sure the final product shines. Automation is my playground, and I’m all about keeping things fast, smooth, and reliable.
+        Let’s connect, geek out on code, or talk automation hacks. I’m all about leveling up and helping the team ship quality stuff!
+      </motion.p>
+
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default SectionWrapper(About, 'about');
---
+++ src/constants/index.js
@@ -0,0 +1,257 @@
+import {
+  frontend,
+  backend,
+  ux,
+  prototyping,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  git,
+  figma,
+  docker,
+  postgresql,
+  rubyrails,
+  graphql,
+  komikult,
+  leaderboard,
+  math,
+  movie,
+  nyeusi,
+  space,
+  coverhunt,
+  dcc,
+  kelhel,
+  microverse,
+} from '../assets';
+
+export const navLinks = [
+  {
+    id: 'about',
+    title: 'About',
+  },
+  {
+    id: 'projects',
+    title: 'Projects',
+  },
+  {
+    id: 'contact',
+    title: 'Contact',
+  },
+];
+
+const services = [
+  {
+    title: 'Manual/Automation Testing & Defect Tracking',
+    icon: frontend,
+  },
+  {
+    title: 'API Testing',
+    icon: backend,
+  },
+  {
+    title: 'Web Development',
+    icon: ux,
+  },
+  {
+    title: 'Mobile App Testing',
+    icon: prototyping,
+  },
+];
+
+const technologies = [
+  {
+    name: 'HTML 5',
+    icon: html,
+  },
+  {
+    name: 'CSS 3',
+    icon: css,
+  },
+  {
+    name: 'JavaScript',
+    icon: javascript,
+  },
+  {
+    name: 'TypeScript',
+    icon: typescript,
+  },
+  {
+    name: 'React JS',
+    icon: reactjs,
+  },
+  {
+    name: 'Tailwind CSS',
+    icon: tailwind,
+  },
+  {
+    name: 'Node JS',
+    icon: nodejs,
+  },
+  {
+    name: 'graphql',
+    icon: graphql,
+  },
+  {
+    name: 'postgresql',
+    icon: postgresql,
+  },
+  {
+    name: 'git',
+    icon: git,
+  },
+  {
+    name: 'figma',
+    icon: figma,
+  },
+  {
+    name: 'docker',
+    icon: docker,
+  },
+];
+
+const experiences = [
+  {
+    title: 'Manual QA Engineer',
+    company_name: 'Devzon , Freelance',
+    icon: coverhunt,
+    iconBg: '#333333',
+    date: 'Jan 2024 - Aug 2024',
+  },
+  {
+    title: 'Automation Test Engineer ',
+    company_name: 'Infobeans Technology Ltd, Indore',
+    icon: microverse,
+    iconBg: '#333333',
+    date: 'Nov 2022 - April 2023',
+  },
+  {
+    title: 'Software Engineer ',
+    company_name: 'Bectron Technology, Bengaluru',
+    icon: kelhel,
+    iconBg: '#333333',
+    date: 'Jan 2019 - Dec 2021',
+  },
+  
+];
+
+const projects = [
+  {
+    id: 'project-1',
+    name: 'KomiKult',
+    description: 'A comic characters list app that displays Marvel characters.',
+    tags: [
+      {
+        name: 'react',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'mongodb',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'tailwind',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: komikult,
+    repo: 'https://github.com/shaqdeff/KomiKult',
+    demo: 'https://shaqdeff.github.io/KomiKult/',
+  },
+  {
+    id: 'project-2',
+    name: 'Leaderboard',
+    description:
+      'A leaderboard list app that displays scores submitted by different players.',
+    tags: [
+      {
+        name: 'react',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'restapi',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'scss',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: leaderboard,
+    repo: 'https://github.com/shaqdeff/Leaderboard',
+    demo: 'https://shaqdeff.github.io/Leaderboard/',
+  },
+  {
+    id: 'project-3',
+    name: 'Math Magicians',
+    description: 'This is a single-page calculator app built with React',
+    tags: [
+      {
+        name: 'nextjs',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'supabase',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'css',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: math,
+    repo: 'https://github.com/shaqdeff/Math-Magicians',
+    demo: 'https://inspiring-medovik-37d3b3.netlify.app/',
+  },
+  {
+    id: 'project-4',
+    name: 'Movie Metro',
+    description: `A single-page application that allows users to search for any movie or show's ratings and its details.`,
+    tags: [
+      {
+        name: 'nextjs',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'supabase',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'css',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: movie,
+    repo: 'https://github.com/shaqdeff/Movie-Metro',
+    demo: 'https://movie-metro.netlify.app/',
+  },
+  {
+    id: 'project-5',
+    name: 'Nyeusi Fest Site',
+    description:
+      'This is a demo concert website for a music festival called Nyeusi.',
+    tags: [
+      {
+        name: 'nextjs',
+        color: 'blue-text-gradient',
+      },
+      {
+        name: 'supabase',
+        color: 'green-text-gradient',
+      },
+      {
+        name: 'css',
+        color: 'pink-text-gradient',
+      },
+    ],
+    image: nyeusi,
+    repo: 'https://github.com/shaqdeff/Nyeusi-Fest-Site',
+    demo: 'https://shaqdeff.github.io/Nyeusi-Fest-Site/',
+  },
+];
+
+export { services, technologies, experiences, projects };
